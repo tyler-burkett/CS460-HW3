3e690745ca9576262412c8f73590fffefaeb5784
Tyler Burkett <36940296+tyler-burkett@users.noreply.github.com>
2020-03-17 19:22:40 -0400

    Added extra regression object that uses interpolation to pick initial weights

--- regression_predictor.py
+++ regression_predictor.py
@@ -1,6 +1,7 @@
 import random
 import time
 import numpy as np
+import math
 from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
 from calculations import mean_square_error
@@ -32,8 +33,6 @@ class RegressionPredictor:
         y = train_data[:, 1]
 
         old_learning_rate = self.learning_rate
-        old_mse = 0
-        new_mse = 0
         start = time.time()
         old_thetas = np.array(self.thetas) * 2
         iterations = 0
@@ -42,7 +41,6 @@ class RegressionPredictor:
             old_thetas = np.array(self.thetas)
             results = self.numeric_func(x)
             self.update_weights(results, y, x)
-            old_mse = new_mse
             iterations = iterations + 1
             self.learning_rate = old_learning_rate / (1 + iterations/self.T)
             self.lambdify_expression()
@@ -75,3 +73,53 @@ class RegressionPredictor:
         substitutions = dict(substitutions)
         x = symbols("x")
         self.numeric_func = lambdify(x, self.expr.subs(substitutions), "numpy")
+
+
+class InterpolatingRegressionPredictor(RegressionPredictor):
+    def fit(self, train_data, order, min_iterations=float("inf"), error_bound=0.01, timeout=60):
+        # build symbolic expression to evaluate polynomial expression
+        self.expr = parse_expr("+".join("N_{}*x**{}".format(i, i) for i in range(order+1)))
+        self.order = order
+
+        # Create inital paramters for polyonomial of provided order
+        # by interpolating points from dataset
+        self.thetas = self.interpolated_weights(train_data)
+
+        x = train_data[:, 0]
+        y = train_data[:, 1]
+
+        old_learning_rate = self.learning_rate
+        start = time.time()
+        old_thetas = np.array(self.thetas) * 2
+        iterations = 0
+        self.lambdify_expression()
+        while abs(max(self.thetas - old_thetas)) > error_bound or iterations < min_iterations:
+            old_thetas = np.array(self.thetas)
+            results = self.numeric_func(x)
+            self.update_weights(results, y, x)
+            iterations = iterations + 1
+            self.learning_rate = old_learning_rate / (1 + iterations/self.T)
+            self.lambdify_expression()
+            if timeout is not None and time.time() - start > timeout \
+                    and (min_iterations == float("inf") or iterations > min_iterations):
+                break
+        self.learning_rate = old_learning_rate
+
+    def interpolated_weights(self, data):
+        # To get unique interpolating polynomial of order k,
+        # need to select k+1 points
+        num_points = self.order + 1
+
+        # Sort data by x value and pick enough points
+        # to uniquely interpolate data, picking evenly spaced points
+        data = np.sort(data, axis=0)
+        selected_idx = np.round(np.linspace(0, len(data)-1, num_points)).astype(int)
+        selected_points = data[selected_idx]
+
+        # Cacluate weights of interpolating polynomial using in-place divided difference
+        thetas = list(selected_points[i][1] for i in range(len(selected_points)))
+        for i in range(0, len(selected_points)-1):
+            for j in range(len(selected_points)-1, i, -1):
+                thetas[j] = (thetas[j] - thetas[j-1]) / (selected_points[j][0] - selected_points[j-i-1][0])
+
+        return np.array(thetas)
--- regression_test.py
+++ regression_test.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from regression_predictor import RegressionPredictor
+from regression_predictor import RegressionPredictor, InterpolatingRegressionPredictor
 from calculations import mean_square_error
 
 if __name__ == "__main__":
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     for i in range(1, 4):
         synth_data.append(np.genfromtxt("./data/synthetic-{}.csv".format(i), delimiter=","))
 
-    fig = plt.subplot(2,2,1)
+    # RegressionPredictor
+    fig = plt.subplot(2, 2, 1)
 
     data_index = 1
     for data in synth_data:
@@ -21,6 +22,36 @@ if __name__ == "__main__":
         for order in [1, 2, 4, 7]:
             # Train regression predictor and measure error
             predictor = RegressionPredictor(1*10**-3, 1000)
+            predictor.fit(data, order, min_iterations=1000, error_bound=0.0001, timeout=3*60)
+            results = list(map(predictor.predict, data[:, 0]))
+            results = np.array(results)
+            print("synthetic-{} data, order={}:".format(data_index, order))
+            print(predictor.predict_func())
+            print(mean_square_error(data[:, 1], results))
+            print("")
+
+            # Plot points and line
+            line_points = np.linspace(data[:, 0].min(), data[:, 0].max(), 1000)
+            line = list(map(predictor.predict, line_points))
+            line = np.array(line)
+            plt.plot(line_points, line, '-', label="{}".format(order))
+        data_index = data_index + 1
+        plt.legend(title="Order")
+    plt.show()
+
+    # InterpolatingRegressionPredictor
+    fig = plt.subplot(2, 2, 1)
+
+    data_index = 1
+    for data in synth_data:
+        plt.subplot(2, 2, data_index)
+        plt.title("synthetic-{} Data and Regression Lines".format(data_index))
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.plot(data[:, 0], data[:, 1], 'go')
+        for order in [1, 2, 4, 7]:
+            # Train regression predictor and measure error
+            predictor = InterpolatingRegressionPredictor(1*10**-3, 1000)
             predictor.fit(data, order, min_iterations=1000, error_bound=0.0001, timeout=3*60)
             results = list(map(predictor.predict, data[:, 0]))
             results = np.array(results)
